be4d502de471a337fae0986cdf375b0a54ee9d72
s <fedor.tryfanau@gmail.com>
2022-06-18 18:31:19 +0300

    added handling of notifications

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -2,9 +2,9 @@
 import * as path from 'path';
 
 import { workspace, Disposable, ExtensionContext } from 'vscode';
-import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, ErrorAction, ErrorHandler, CloseAction, TransportKind } from 'vscode-languageclient/node';
+import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, DocumentSelector, ErrorAction, ErrorHandler, CloseAction, TransportKind } from 'vscode-languageclient/node';
 
-function startLangServer(command: string, args: string[], documentSelector: string[]): Disposable {
+function startLangServer(command: string, args: string[], documentSelector: DocumentSelector | string[]): Disposable {
 	const serverOptions: ServerOptions = {
 		command,
 		args,
@@ -20,7 +20,7 @@ export function activate(context: ExtensionContext) {
 	if (false) {// if debug
 		args = ['tmux', 'new-session', '-d', '-s', 'language-server', args.join(' ')]
 	}
-	context.subscriptions.push(startLangServer(args[0], args.slice(1), ["jararaca"]));
+	context.subscriptions.push(startLangServer(args[0], args.slice(1), [{ scheme: 'file', language: 'jararaca' }]));
 	// For TCP server needs to be started separately
 	// context.subscriptions.push(startLangServerTCP(2087, ["python"]));
 }
--- server/server.py
+++ server/server.py
@@ -3,13 +3,13 @@ import logging as logs
 import sys
 import os
 import traceback
-from typing import Any
+from typing import Any, NoReturn
 
 def main() -> None:
 	dir = os.path.dirname(os.path.realpath(__file__))
 	logs.basicConfig(
 		filename=os.path.join(dir,f"server.logs"),
-		filemode='a',
+		filemode='w',
 		format=f'%(asctime)s,%(msecs)d, {os.getpid()} %(name)s %(levelname)s %(message)s',
 		datefmt='%H:%M:%S',
 		level=logs.DEBUG)
@@ -17,7 +17,7 @@ def main() -> None:
 	
 	try:
 		while True:
-			handle_request(get_json())
+			handle_content(get_json())
 	except Exception:
 		logs.error(traceback.format_exc())
 	logs.debug("Server stopped")
@@ -36,8 +36,7 @@ def get_json() -> Any:
 		if name == 'Content-Length':
 			length = int(body)
 		else:
-			logs.error(f"Unknown Header: {name = !r}, {body = !r}")
-			sys.exit(1)
+			exit_abnormally(f"Unknown Header: {name = !r}, {body = !r}")
 	length = length
 	content_string = sys.stdin.read(length)
 	content = json.loads(content_string)
@@ -50,6 +49,7 @@ def send_msg(msg:dict[str, Any]) -> None:
 	length = len(content)
 	sys.stdout.write(f"Content-Length: {length}\r\n")
 	sys.stdout.write(f"\r\n{content}")
+	sys.stdout.flush()
 	logs.debug(f"Sent: {content}")
 integer = int
 class ErrorCodes:
@@ -76,39 +76,85 @@ class TextDocumentSyncKind:
 	Full = 1
 	Incremental = 2
 
-def handle_request(request:dict[str,Any]) -> None:
-	if request["jsonrpc"] != "2.0":
-		logs.error(f"Expected jsonrpc to be 2.0")
-		sys.exit(1)
-	id:int = request["id"]
-	params = request["params"]
-	method:str = request["method"]
+def exit_abnormally(msg:str) -> NoReturn:
+	logs.error(msg)
+	publish_notification('exit', None)
+	print(f"Server exited abnormally, due to {msg!r}", file=sys.stderr, flush=True)
+	sys.exit(1)
 
-	result:dict[str,Any]
+def handle_content(request:dict[str,Any]) -> None:
+	if request["jsonrpc"] != "2.0":
+		exit_abnormally(f"Expected jsonrpc to be 2.0")
+	params:None|dict[str,Any]|list[Any] = request.get("params")
+	method:str = request["method"]
+	id:int|str = request.get("id")
+	if id is None:
+		logs.debug(f"Received notification '{method}'")
+		return handle_notification(method, params)
+	logs.debug(f"Received request #{id} '{method}'")
+	return handle_request(id, method, params)
+
+
+def publish_notification(method:str, params:Any) -> None:
+	send_msg({"method":method,"params":params})
+
+def compute_diagnostics() -> None:
+	assert False, "Not implemented"
+
+def handle_request(id:int|str, method:str, params:None|dict[str,Any]|list[Any]) -> None:
+	def reply(result:Any) -> None:
+		return send_msg({"id":id,"result":result})
+	def error(code:integer, message:str) -> None:
+		return send_msg({"id":id,"error":{"code":code,"message":message}})
 	if method == 'initialize':
-		result = {
-			"capabilities": {
-				"textDocumentSync":TextDocumentSyncKind.Incremental,
-				"workspace":{
-					"workspaceFolders":{
-						"supported":True,
-					}
+		logs.info("Received initialize request")
+		CAPABILITIES = {
+				"textDocumentSync":TextDocumentSyncKind.Full,
+				"diagnosticProvider":{
+					"interFileDependencies":True,
+					"workspaceDiagnostics":True,
 				}
 			}
-		}
+		return reply({
+			"capabilities": CAPABILITIES,
+		})
+	elif method == 'shutdown':
+		logs.info("Received shutdown request, shutting down")
+		return reply(None)
+	elif method == 'exit':
+		logs.info("Received exit request, server exits now (with 0)")
+		sys.exit(0)
 	else:
-		logs.error(f"Unknown method: {method!r}, params: {json.dumps(params, indent=4)}")
-		return send_msg({
-		"id": id,
-		"error":{
-			"code":ErrorCodes.MethodNotFound,
-			"message":0,
-		}
-	})
-	send_msg({
-		"id": id,
-		"result":result
-	})
+		logs.error(f"Received unknown method: {method!r}, params: {json.dumps(params, indent=4)}")
+		return error(ErrorCodes.MethodNotFound, f"method '{method}' can't be handled on the server")
+
+def handle_notification(method:str, params:Any) -> None:
+	if method == 'initialized':
+		logs.info("Received initialized notification, all good")
+	elif method == 'textDocument/didSave':#ignore
+		compute_diagnostics()
+	elif method == 'textDocument/didOpen':
+		text = params["textDocument"]["text"]
+		if params["textDocument"]["languageId"] != 'jararaca':
+			exit_abnormally(f"Received a request for another language, exiting")
+		uri = params["textDocument"]["uri"]
+		OPENED_FILES_TEXTS[uri] = text
+		logs.info(f"Opened file {uri!r}")
+	elif method == 'textDocument/didChange':
+		text = params["contentChanges"][-1]["text"] # we need to apply all changes, so we take the last one's text 
+		uri = params["textDocument"]["uri"]
+		OPENED_FILES_TEXTS[uri] = text
+		logs.debug(f"Changed file {uri!r}")
+	elif method == 'textDocument/didClose':
+		uri = params["textDocument"]["uri"]
+		del OPENED_FILES_TEXTS[uri]
+		logs.info(f"Closed file {uri!r}")
+	elif method.startswith('$/'):#ignore
+		return
+	else:
+		logs.warn(f"Received unknown notification: {method!r}, go check")
+
+OPENED_FILES_TEXTS:dict[str,str] = {}
 
 if __name__ == '__main__':
 	main()
